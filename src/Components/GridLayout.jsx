5a75e1ce7b46b3c6b65595239aaf7b55c30b2126
unknown <salihu45@gmail.com>
2022-01-12 20:18:31 +0100

    design

--- src/Components/GridLayout.jsx
+++ src/Components/GridLayout.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-// import { Box } from "@chakra-ui/react";
+import Daniel from "../images/image-daniel.jpg";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import { Center } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
-
-const sizes = {
-  space: {
-    px: "1px",
-    32: "8rem",
-  },
-};
 
 const GridLayout = () => {
   return (
@@ -17,7 +13,7 @@ const GridLayout = () => {
         <Grid
           w="90%"
           maxW="1080px"
-          my="11rem"
+          my="7rem"
           templateColumns="repeat(5,1fr)"
           gap={10}
         >
@@ -25,18 +21,29 @@ const GridLayout = () => {
             rowSpan={1}
             colSpan={3}
             bgColor=" hsl(263, 55%, 52%)"
-            bgImage="url('../src/images/bg-pattern-quotation.svg')"
+            bgImage="url('../src')"
             borderRadius="5px"
             bgRepeat="no-repeat"
+            p="1.3em"
           >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cum
-              sit tempora! Rem tenetur doloribus provident ipsum quam odio
-              officiis nemo unde atque tempora ab nesciunt earum nostrum, porro
-              molestiae. Lorem, ipsum dolor sit amet consectetur adipisicing
-              elit. Quae veniam unde harum, minus minima necessitatibus est
-              magni voluptatem animi modi?
-            </p>
+            <Flex>
+              <Center flex="1" w="100%">
+                <Image src={Daniel} borderRadius="50%" alt="Daniel-avatar" />
+              </Center>
+              <Box flex="7">
+                <Text mt="0.4rem"> Daniel Clifford</Text>
+                <Text mt="0.2rem"> Daniel Clifford</Text>
+              </Box>
+            </Flex>
+            <Text mt = "0.8rem">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </Text>
           </GridItem>
           <GridItem bgColor="hsl(217, 19%, 35%)" borderRadius="5px">
             <p>
